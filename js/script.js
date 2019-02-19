4081d6a62286b0c7947dec6b2d880f473c16a3f0
v.lvov <lvov.86@mail.ru>
2019-02-19 12:05:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-const cartWrapper = 
+const cartWrapper = document.querySelector('.cart__wrapper'),
+	cart = document.querySelector('.cart'),
+	close = document.querySelector('.cart__close'),
+	open = document.querySelector('#cart'),
+	goodsBtn = document.querySelectorAll('.goods__btn'),
+	products = document.querySelectorAll('.goods__item'),
+	confirm = document.querySelector('.confirm'),
+	badge= document.querySelector('.nav__bage'),
+	totalCost = document.querySelector('.cart__total > span'),
+	titles = document.querySelectorAll('.doods__title');
