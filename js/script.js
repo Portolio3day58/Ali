47c609f7bac60a4929fdeb6f47664b0c78bebce9
AliExpress <47081831+Portolio3day58@users.noreply.github.com>
2019-02-21 11:01:16 +0300

    push

--- js/script.js
+++ js/script.js
@@ -46,16 +46,18 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 
-	titles.forEach(function (item) {
-		if (item.textContent.length < 70) {
-			return;
-		} else {
-			const  str = item.textContent.slice(0, 71) + '...';
-			//const  str = `${item.textContent.slice(0, 71)}...`;
-			item.textContent = str;
-		}
-    });
-
+	function sliceTitle() {
+        titles.forEach(function (item) {
+            if (item.textContent.length < 70) {
+                return;
+            } else {
+                const str = item.textContent.slice(0, 71) + '...';
+                //const  str = `${item.textContent.slice(0, 71)}...`;
+                item.textContent = str;
+            }
+        });
+    }
+	sliceTitle();
 
 
 
